56e5c1b79cf36d49ac8d156d441f91eacac6c9cc
Noor <noor@gmail.com>
2026-04-23 15:47:38 +0530

    Added device code in csv.py

--- Lessons/Lesson5_inventory/multidevice_connection_read_csv.py
+++ Lessons/Lesson5_inventory/multidevice_connection_read_csv.py
@@ -1,4 +1,10 @@
-import os
+import netmiko
 
 
-print(os.getcwd())
+device = {
+    'device_type': 'cisco_ios',
+    'ip': '172.16.31.10',
+    'username': 'admin',
+    'password': 'admin',
+    'secret':'admin'
+}
